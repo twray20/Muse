45f9dcea6c387dd1bfcbdb75fff3165ade8ffd9d
twray20 <thomas.wray@cox.net>
2017-11-04 19:54:08 -0500

    Initializing the REPO
    Starting the repository up.
    
    Added basic song class
    Added some general functions that need to be accessible form multiple
    points

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Global.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class Global
+    {
+        public Song[] songList;
+
+        public Song getSong(int songID)
+        {
+            foreach(Song song in songList)
+            {
+                if (songID == song.getID())
+                {
+                    return song;
+                }
+            }
+            return new Song();
+        }
+
+        public void changeRating(int songID, int rating)
+        {
+            Song song = getSong(songID);
+            int newNum, newAvg;
+            newNum = song.getNumRatings();
+            newAvg = song.getAvgRating();
+            newAvg = ((newAvg * newNum) + rating) / (newNum + 1);
+            song.setAvgRating(newAvg);
+            song.setNumRatings(newNum + 1);
+        }
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Song.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class Song
+    {
+        private int songID;     //Unique song ID to identify the song and retrieve information about it
+        private String songName;
+        private String artist;
+        private int songLength;
+        private int songYear;
+        //private Descriptor[] descriptors;
+        private int numRatings;         //How many people have rated the song
+        private int avgRating;          //Average total rating of the song
+
+        public int getID()
+        {
+            return songID;
+        }
+
+        public String getName()
+        {
+            return songName;
+        }
+
+        public String getArtist()
+        {
+            return artist;
+        }
+
+        public int getSongLength()
+        {
+            return songLength;
+        }
+
+        public int getSongYear()
+        {
+            return songYear;
+        }
+
+        //public Descriptor[] getDescriptors()
+        //{
+        //    return descriptors;
+        //}
+
+        public int getNumRatings()
+        {
+            return numRatings;
+        }
+
+        public int getAvgRating()
+        {
+            return avgRating;
+        }
+
+        public void setAvgRating(int newAvg)
+        {
+            this.avgRating = newAvg;
+        }
+
+        public void setNumRatings(int newNum)
+        {
+            this.numRatings = newNum;
+        }
+
+
+
+    }
+}
